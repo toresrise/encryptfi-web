c9c7781b596e85a18ea070e191358faefe22eca9
hotequil <hotequil@protonmail.com>
2022-07-25 21:24:20 -0300

    style: removed semicolon and done lint fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMeta {
     env: {
-        VITE_API_URL: string;
-        VITE_LOCAL_STORAGE_PREFIX: string;
-        VITE_LANG_KEY: string;
-    };
+        VITE_API_URL: string
+        VITE_LOCAL_STORAGE_PREFIX: string
+        VITE_LANG_KEY: string
+    }
 }
