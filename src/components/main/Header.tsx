beebd3f0eaa0b59d8727cfaa2e79526f563c00e2
hotequil <hotequil@protonmail.com>
2022-07-30 00:04:29 -0300

    feat: init theme toggle

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,5 +1,8 @@
 import { useTranslation } from 'react-i18next'
 import styles from './Header.module.scss'
+import { ThemeHelper } from '~/helpers/theme'
+
+const themeHelper = new ThemeHelper()
 
 export const Header = () => {
     const { t } = useTranslation()
@@ -8,7 +11,9 @@ export const Header = () => {
         <header className={styles.header}>
             <img className={styles.logo} src='/src/assets/images/logo.png' alt={t('app.logo')} />
             <h1 className={styles.title}>{t('app.name')}</h1>
-            <div className={styles.actions}>TODO ACTIONS</div>
+            <div className={styles.actions}>
+                <button onClick={themeHelper.toggle.bind(themeHelper)}>TODO CHANGE THEME</button>
+            </div>
         </header>
     )
 }
