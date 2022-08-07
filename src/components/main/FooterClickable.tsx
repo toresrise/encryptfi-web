c3db23ff6b75b183c5bb563dbd70a3b3031be2f8
hotequil <hotequil@protonmail.com>
2022-08-07 14:05:56 -0300

    feat: added className in FooterClickable

--- src/components/main/FooterClickable.tsx
+++ src/components/main/FooterClickable.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import styles from './FooterClickable.module.scss'
 
@@ -12,20 +12,22 @@ interface Props {
     layout?: FooterClickableLayout
     onClick?: () => void
     href?: string
+    className?: string
 }
 
 export const FooterClickable: React.FC<Props> = ({ text, layout = FooterClickableLayout.BUTTON, ...props }) => {
     const { t } = useTranslation()
+    const className = useMemo(() => `${styles.clickable} ${props.className ?? ''}`, [props.className])
 
     return (
         <>
             {layout === FooterClickableLayout.BUTTON && (
-                <button type='button' {...props} className={styles.clickable}>
+                <button type='button' {...props} className={className}>
                     {t(text)}
                 </button>
             )}
             {layout === FooterClickableLayout.ANCHOR && (
-                <a rel='noreferrer' target='_self' {...props} className={styles.clickable}>
+                <a rel='noreferrer' target='_self' {...props} className={className}>
                     {t(text)}
                 </a>
             )}
