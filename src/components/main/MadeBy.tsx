59a09669ce84bb997350933c96c2a5773555e927
hotequil <hotequil@protonmail.com>
2022-08-07 15:35:51 -0300

    feat: added logos in scripts

--- src/components/main/MadeBy.tsx
+++ src/components/main/MadeBy.tsx
@@ -1,12 +1,13 @@
 import { useTranslation } from 'react-i18next'
 import styles from './MadeBy.module.scss'
+import toresriseLogo from '~/assets/images/toresrise.png'
 
 export const MadeBy = () => {
     const { t } = useTranslation()
 
     return (
         <div className={styles.madeBy}>
-            <img className={styles.logo} src='/src/assets/images/toresrise.png' alt={t('toresrise.logo')} />
+            <img className={styles.logo} src={toresriseLogo} alt={t('toresrise.logo')} />
             <p className={styles.text}>
                 {t('madeBy.text')}{' '}
                 <a className={styles.link} href='https://github.com/toresrise' target={'_blank'} rel='noreferrer'>
