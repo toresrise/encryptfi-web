11daa242684559846e8bdcc15443b408ad659c61
hotequil <hotequil@protonmail.com>
2022-08-14 21:58:19 -0300

    feat: init Upload component

---
+++ src/components/upload/Upload.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TitleSection } from '~/components/global/TitleSection'
+
+export const Upload: React.FC = () => (
+    <section>
+        <TitleSection title={'upload.title'} />
+    </section>
+)
